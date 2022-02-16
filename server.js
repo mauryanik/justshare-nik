4954a31da4f15a326091efcb47f51c36548d4269
Nikesh Maurya <nikeshinknit@gmail.com>
2022-02-16 10:21:22 +0530

    urlencoded true

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const connectDB = require('./config/db');
 connectDB();
 
